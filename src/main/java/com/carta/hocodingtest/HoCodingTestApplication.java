84d679bd3e528585f25e29871940914113692da7
HO <>
2022-02-20 11:19:17 -0300

    init

---
+++ src/main/java/com/carta/hocodingtest/HoCodingTestApplication.java
@@ -0,0 +1,13 @@
+package com.carta.hocodingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoCodingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoCodingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carta/hocodingtest/HoCodingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carta.hocodingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoCodingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
